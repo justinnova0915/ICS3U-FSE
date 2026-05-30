e62d5075766ab4533c7f5a80eddd9922a7e68439
Justin Li <justinnova0915@gmail.com>
2026-05-30 17:40:17 -0400

    IDEKBRO

--- ui/animator.py
+++ ui/animator.py
@@ -10,57 +10,38 @@ class Animator:
     def __init__(self):
         self.elapsed = 0
         self.progress = 0
-        self.active = []
+        self.tiles: list[list[Tile]] = []
 
-    def load_queue(self, moved: list[Tile]):
-        for tile in moved:
-
-            start = (float(self._gridToPixel(tile.start[0])), 
-                    float(self._gridToPixel(tile.start[1])))
-            end   = (float(self._gridToPixel(tile.end[0])), 
-                    float(self._gridToPixel(tile.end[1])))
-            
-            self.active.append(Tile(
-                value   = tile.value,
-                curr    = start,
-                prev    = end,
-                merged  = tile.merged
-            ))
-    
-
-
-    def update(self, dt: float):
-        self.elapsed += dt
-        progress = min(self.elapsed/ANIM_DURATION, 1.0)
-        result = []
-
-        if progress == 1.0:
-            self.active = []
-        
-        for tile in self.active:
-            lerpedPos = self._lerp(tile.sPos, tile.ePos, progress)
-            result.append(Tile(
-                value = tile.value,
-                pos   = lerpedPos
-            ))
-
-    def get_active(self) -> list[Tile]:
-        return self.active
-    
-
-    def _gridToPixel(self, rowIndex: int, colIndex: int) -> Coord:
-        ''' Converts a grid coordinate to a pixel coordinate '''
-        x = CELL_PAD.w + (CELL_SIZE.w + CELL_PAD.w) * colIndex
-        y = CELL_PAD.h + (CELL_SIZE.h + CELL_PAD.h) * rowIndex
-        return Coord(x, y)
-    
-    def _lerp(self, start: tuple[float, float], end: tuple[float, float], t: float) -> float:
-        return tuple(
-            start[0] + (end[0]-start[0]) * self._ease(t),
-            start[1] + (end[1]-start[1]) * self._ease(t)
-        )
+    def load_queue(self, board: list[list[Tile]]):
+        for tile in board:
+            self.tiles.append(tile)
     
     def _ease(progress: float) -> float:
         c1 = 1.1
         c3 = c1 + 1.0
         return 1.0 + c3 * ((progress - 1.0) ** 3) + c1 * ((progress - 1.0) ** 2)
+
+    def _lerp(self, start: tuple[float, float], end: tuple[float, float], progess: float) -> float:
+        eased_progress = self._ease(progess)
+        return tuple(
+            start[0] + (end[0]-start[0])*eased_progress,
+            start[1] + (end[1]-start[1])*eased_progress
+        )
+
+    def update(self, dt: float) -> list[list[Tile]]:
+        self.elapsed += dt
+        progress = min(self.elapsed/ANIM_DURATION, 1.0)
+        board = []
+
+        if progress == 1.0:
+            self.tiles = []
+        
+        for r in self.tiles:
+            for tween in r:
+                tween.pos = self._lerp(tween.curr, tween.p, progress)
+                board.append(tween)
+        
+        return board
+
+    def get_active(self) -> list[Tile]:
+        return self.active
